9cf088d3de0133f03729fb812b57aa708392acab
Crowdbotics <team@crowdbotics.com>
2020-12-03 00:33:49 +0000

    Update models

--- ultra_dm_23141/wsgi.py
+++ ultra_dm_23141/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ultra_dm_23141.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ultra_dm_23141.settings")
 
 application = get_wsgi_application()
